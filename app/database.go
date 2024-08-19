e3c66da60c62fdf5326086b5666d58d6d1e2280c
roniragilimankhoirul <ronisensei7@gmail.com>
2024-08-19 20:28:06 +0700

    Feat: Added Database Configuraton

---
+++ app/database.go
@@ -0,0 +1,18 @@
+package app
+
+import (
+	"belajar-golang-rest-api/helper"
+	"database/sql"
+	"time"
+)
+
+func NewDB() *sql.DB {
+	db, err := sql.Open("pgx", "postgres://postgres:@localhost:5432/belajar_golang_rest-api")
+	helper.PanicIfError(err)
+
+	db.SetMaxIdleConns(5)
+	db.SetMaxOpenConns(10)
+	db.SetConnMaxLifetime(60 * time.Minute)
+	db.SetConnMaxIdleTime(10 * time.Minute)
+	return db
+}
